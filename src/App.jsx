9c9d3ca3f2f07895aa26d5bf8595383a5a4a1e3f
gourranz <Zakaria.Gourrane@lions.enc.edu>
2024-02-12 00:20:37 -0500

    App

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router';
+import Nav from './components/Nav';
+import Register from './pages/Register';
+import SignIn from './pages/SignIn';
+import Home from './pages/Home';
+import './App.css';
+import { CheckSession } from './services/Auth.js';
+import Quiz from './pages/QuizList.jsx'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [user, setUser] = useState(null);
+
+  const checkToken = async () => {
+    try {
+      const user = await CheckSession();
+      setUser(user);
+    } catch (error) {
+      // Handle any errors from CheckSession
+      console.error('Error checking session:', error);
+    }
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    // Check if token exists before requesting to validate the token
+    if (token) {
+      checkToken();
+    }
+  }, []);
+
+  const handleLogOut = () => {
+    // Reset all auth-related state and clear localStorage
+    setUser(null);
+    localStorage.clear();
+  };
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <Nav user={user} handleLogOut={handleLogOut} />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<SignIn setUser={setUser} />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/quiz" element={<Quiz />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
-export default App
+export default App;

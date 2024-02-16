a40fea735ba49c735294327064d640fd31108e57
gourranz <Zakaria.Gourrane@lions.enc.edu>
2024-02-16 10:10:11 -0500

    Readme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,21 @@
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../App.css'
 
 const Home = () => {
-  let navigate = useNavigate()
+  const navigate = useNavigate();
 
   return (
     <div className="home-container col">
-
+      
       <section className="welcome-signin">
+        
         <button onClick={() => navigate('/signin')}>
           Click Here To Get Started
         </button>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
